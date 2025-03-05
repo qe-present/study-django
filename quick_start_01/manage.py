b537a44f61c8b52686ccb524baadc923d06098b2
qe-present <2664481691@qq.com>
2025-03-06 01:07:52 +0800

    add

--- quick_start_01/manage.py
+++ quick_start_01/manage.py
@@ -2,7 +2,8 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+import webbrowser
+import threading
 
 def main():
     """Run administrative tasks."""
@@ -15,6 +16,12 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    def open_browser():
+        webbrowser.open_new('http://127.0.0.1:8000')
+
+    if 'runserver' in sys.argv:
+        threading.Timer(1.25, open_browser).start()
+
     execute_from_command_line(sys.argv)
 
 
